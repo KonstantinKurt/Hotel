1fec1919639fc14f54f66bb03caeaff2049f311e
KonstantinKurt <kurbatskykv@gmail.com>
2019-05-06 11:43:16 +0300

    token

--- libs/enshureTokenWithBarrier.js
+++ libs/enshureTokenWithBarrier.js
@@ -3,11 +3,10 @@ module.exports = function(req, res, next) {
     const bearerHeader = req.headers['Authorization'];
     if (typeof bearerHeader !== 'undefined') {
         const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
+        req.token = bearer[1];
         next();
     } else {
-        res.sendStatus(403);
+        res.staus(403).json({message: "invalid token"});
     }
 };
 
