1489181fcd13421a71bfc4c03eeb119f407c4bfa
KonstantinKurt <kurbatskykv@gmail.com>
2019-05-06 11:33:19 +0300

    Authorization

--- libs/enshureTokenWithBarrier.js
+++ libs/enshureTokenWithBarrier.js
@@ -1,6 +1,6 @@
 
 module.exports = function(req, res, next) {
-    const bearerHeader = req.headers['authorization'];
+    const bearerHeader = req.headers['Authorization'];
     if (typeof bearerHeader !== 'undefined') {
         const bearer = bearerHeader.split(' ');
         const bearerToken = bearer[1];
